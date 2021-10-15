48bd8d03f215720e2c7d6149d83257c914f80ad3
Divine-Princess <tor20023@byui.edu>
2021-10-14 18:16:50 -0700

    incomplete

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -1,4 +1,6 @@
 function toggleMenu() {
     document.getElementById("navigation").classList.toggle("hide");
 }
-document.querySelector("#copyrightyear").textContent = new Date().toLocaleDateString('en-gb', { weekday:"long", year:"numeric", day:"numeric", month:"short"}) 
+
+document.querySelector("#copyrightyear").textContent = new Date().toLocaleDateString('en-gb', { weekday:"long", year:"numeric", day:"numeric", month:"short"});
+
