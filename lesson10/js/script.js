a23c96db156706278d243a15b56346d5c043d4c1
Divine-Princess <tor20023@byui.edu>
2021-11-18 16:47:10 -0800

    preston api

---
+++ lesson10/js/script.js
@@ -0,0 +1,23 @@
+const today = new Date();
+
+const dayNumber = today.getDay();
+
+const element = document.getElementById("message");
+
+if (dayNumber == 5) {
+    element.classList.add("showme");
+} else {
+    element.classList.add("hideme");
+}
+
+function toggleMenu() {
+    document.getElementById("navigation").classList.toggle("hide");
+}
+
+document.querySelector("#copyrightyear").textContent = new Date().toLocaleDateString('en-gb', { weekday:"long", year:"numeric", day:"numeric", month:"short"});
+
+// Storm Severity
+function adjustSeverity(severity) {
+    document.getElementById("severityvalue").innerHTML = severity;
+}
+
---
+++ lesson10/js/webfontloader.js
@@ -0,0 +1,1 @@
+WebFont.load({google: {families: ["Lato", "Oleo Script", "Slabo 27px" ]} });
